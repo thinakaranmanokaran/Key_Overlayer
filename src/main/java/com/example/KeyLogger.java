ee39b407bcd5e9b643cf40ce983aea9bc3848657
thinakaranmanokaran <thinakaranmanohar@yahoo.com>
2025-11-21 16:15:10 +0530

    Ok

--- src/main/java/com/example/KeyLogger.java
+++ src/main/java/com/example/KeyLogger.java
@@ -4,12 +4,15 @@ import com.github.kwhat.jnativehook.GlobalScreen;
 import com.github.kwhat.jnativehook.keyboard.NativeKeyEvent;
 import com.github.kwhat.jnativehook.keyboard.NativeKeyListener;
 
+import java.util.HashSet;
+import java.util.Set;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
 public class KeyLogger implements NativeKeyListener {
 
     private final OverlayWindow overlayWindow;
+    private final Set<String> activeModifiers = new HashSet<>();
 
     public KeyLogger(OverlayWindow overlayWindow) {
         this.overlayWindow = overlayWindow;
@@ -28,11 +31,64 @@ public class KeyLogger implements NativeKeyListener {
         GlobalScreen.addNativeKeyListener(this);
     }
 
-    @Override
-    public void nativeKeyPressed(NativeKeyEvent e) {
-        overlayWindow.displayKey(NativeKeyEvent.getKeyText(e.getKeyCode()));
+    private boolean isModifier(int keyCode) {
+        switch (keyCode) {
+            case NativeKeyEvent.VC_SHIFT:
+            case NativeKeyEvent.VC_CONTROL:
+            case NativeKeyEvent.VC_META:
+            case NativeKeyEvent.VC_ALT:
+                return true;
+            default:
+                return false;
+        }
     }
 
-    @Override public void nativeKeyReleased(NativeKeyEvent e) { }
-    @Override public void nativeKeyTyped(NativeKeyEvent e) { }
+    private String getModifierName(int keyCode) {
+        switch (keyCode) {
+            case NativeKeyEvent.VC_SHIFT:
+                return "Shift";
+            case NativeKeyEvent.VC_CONTROL:
+                return "Ctrl";
+            case NativeKeyEvent.VC_META:
+                return "Meta";
+            case NativeKeyEvent.VC_ALT:
+                return "Alt";
+            default:
+                return "";
+        }
+    }
+
+    @Override
+    public void nativeKeyPressed(NativeKeyEvent e) {
+        int code = e.getKeyCode();
+
+        // If modifier pressed
+        if (isModifier(code)) {
+            activeModifiers.add(getModifierName(code));
+            return;
+        }
+
+        // Normal key pressed
+        String key = NativeKeyEvent.getKeyText(code);
+
+        if (!activeModifiers.isEmpty()) {
+            overlayWindow.displayKey(String.join(" + ", activeModifiers) + " + " + key);
+        } else {
+            overlayWindow.displayKey(key);
+        }
+    }
+
+    @Override
+    public void nativeKeyReleased(NativeKeyEvent e) {
+        int code = e.getKeyCode();
+
+        // If modifier released
+        if (isModifier(code)) {
+            activeModifiers.remove(getModifierName(code));
+        }
+    }
+
+    @Override
+    public void nativeKeyTyped(NativeKeyEvent e) {
+    }
 }
